6fb1baa5cf83f2c9bd4ed5c1c083f2187f50afa8
Eduardo Escoto <eduardo.escoto@noom.com>
2024-01-16 09:52:19 -0800

    added math rendering dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 import {SITE_METADATA} from "./src/consts.ts";
 
@@ -9,5 +11,13 @@ import {SITE_METADATA} from "./src/consts.ts";
 export default defineConfig({
     prefetch: true,
     site: SITE_METADATA.siteUrl,
-    integrations: [mdx(), sitemap(), tailwind()]
+    integrations: [mdx(), sitemap(), tailwind()],
+    markdown: {
+        rehypePlugins: [
+            rehypeKatex
+        ],
+        remarkPlugins: [
+            remarkMath
+        ]
+    }
 });
